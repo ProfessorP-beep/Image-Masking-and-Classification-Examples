a95d2a28c626706f6809202f11822ca112533266
ProfessorP <pwebster26@gmail.com>
2024-02-27 03:00:16 -0500

    Trying Keras example on NIH chest xrays hosted on google.

--- Image_Masking_and_Classification.py
+++ Image_Masking_and_Classification.py
@@ -1,4 +1,211 @@
-import numpy as np
+# Example Taken from https://keras.io/examples/vision/masked_image_modeling/
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
 import tensorflow as tf
 import keras
 from keras import layers
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import kaggle
+
+#Check if our api key form kaggle is configured correctly
+kaggle.config_path = os.path.join(os.path.expanduser('~'), '.kaggle', 'kaggle.json')
+kaggle.KaggleApi.authenticate()
+
+print("Kaggle API is configured successfully!")
+
+
+# Setting seeds for reproducibility.
+SEED = 42
+keras.utils.set_random_seed(SEED)
+
+# DATA
+BUFFER_SIZE = 1024
+BATCH_SIZE = 256
+AUTO = tf.data.AUTOTUNE
+INPUT_SHAPE = (32, 32, 3)
+NUM_CLASSES = 10
+
+# OPTIMIZER
+LEARNING_RATE = 5e-3
+WEIGHT_DECAY = 1e-4
+
+# PRETRAINING
+EPOCHS = 100
+
+# AUGMENTATION
+IMAGE_SIZE = 48  # We will resize input images to this size.
+PATCH_SIZE = 6  # Size of the patches to be extracted from the input images.
+NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
+MASK_PROPORTION = 0.75  # We have found 75% masking to give us the best results.
+
+# ENCODER and DECODER
+LAYER_NORM_EPS = 1e-6
+ENC_PROJECTION_DIM = 128
+DEC_PROJECTION_DIM = 64
+ENC_NUM_HEADS = 4
+ENC_LAYERS = 6
+DEC_NUM_HEADS = 4
+DEC_LAYERS = (
+    2  # The decoder is lightweight but should be reasonably deep for reconstruction.
+)
+ENC_TRANSFORMER_UNITS = [
+    ENC_PROJECTION_DIM * 2,
+    ENC_PROJECTION_DIM,
+]  # Size of the transformer layers.
+DEC_TRANSFORMER_UNITS = [
+    DEC_PROJECTION_DIM * 2,
+    DEC_PROJECTION_DIM,
+]
+
+# Load and prepare the NIH Chest X-Ray Dataset
+kaggle_chest= kaggle.   ('nih/chest-xrays')
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+(x_train, y_train), (x_val, y_val) = (
+    (x_train[:40000], y_train[:40000]),
+    (x_train[40000:], y_train[40000:]),
+)
+print(f"Training samples: {len(x_train)}")
+print(f"Validation samples: {len(x_val)}")
+print(f"Testing samples: {len(x_test)}")
+
+train_ds = tf.data.Dataset.from_tensor_slices(x_train)
+train_ds = train_ds.shuffle(BUFFER_SIZE).batch(BATCH_SIZE).prefetch(AUTO)
+
+val_ds = tf.data.Dataset.from_tensor_slices(x_val)
+val_ds = val_ds.batch(BATCH_SIZE).prefetch(AUTO)
+
+test_ds = tf.data.Dataset.from_tensor_slices(x_test)
+test_ds = test_ds.batch(BATCH_SIZE).prefetch(AUTO)
+
+def get_train_augmentation_model():
+    model = keras.Sequential(
+        [
+            layers.Rescaling(1 / 255.0),
+            layers.Resizing(INPUT_SHAPE[0] + 20, INPUT_SHAPE[0] + 20),
+            layers.RandomCrop(IMAGE_SIZE, IMAGE_SIZE),
+            layers.RandomFlip("horizontal"),
+        ],
+        name="train_data_augmentation",
+    )
+    return model
+
+
+def get_test_augmentation_model():
+    model = keras.Sequential(
+        [
+            layers.Rescaling(1 / 255.0),
+            layers.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+        ],
+        name="test_data_augmentation",
+    )
+    return model
+
+# A layer for extracting patches from images 
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size=PATCH_SIZE, **kwargs):
+        super().__init__(**kwargs)
+        self.patch_size = patch_size
+
+        # Assuming the image has three channels each patch would be
+        # of size (patch_size, patch_size, 3).
+        self.resize = layers.Reshape((-1, patch_size * patch_size * 3))
+
+    def call(self, images):
+        # Create patches from the input images
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+
+        # Reshape the patches to (batch, num_patches, patch_area) and return it.
+        patches = self.resize(patches)
+        return patches
+
+    def show_patched_image(self, images, patches):
+        # This is a utility function which accepts a batch of images and its
+        # corresponding patches and help visualize one image and its patches
+        # side by side.
+        idx = np.random.choice(patches.shape[0])
+        print(f"Index selected: {idx}.")
+
+        plt.figure(figsize=(4, 4))
+        plt.imshow(keras.utils.array_to_img(images[idx]))
+        plt.axis("off")
+        plt.show()
+
+        n = int(np.sqrt(patches.shape[1]))
+        plt.figure(figsize=(4, 4))
+        for i, patch in enumerate(patches[idx]):
+            ax = plt.subplot(n, n, i + 1)
+            patch_img = tf.reshape(patch, (self.patch_size, self.patch_size, 3))
+            plt.imshow(keras.utils.img_to_array(patch_img))
+            plt.axis("off")
+        plt.show()
+
+        # Return the index chosen to validate it outside the method.
+        return idx
+
+    # taken from https://stackoverflow.com/a/58082878/10319735
+    def reconstruct_from_patch(self, patch):
+        # This utility function takes patches from a *single* image and
+        # reconstructs it back into the image. This is useful for the train
+        # monitor callback.
+        num_patches = patch.shape[0]
+        n = int(np.sqrt(num_patches))
+        patch = tf.reshape(patch, (num_patches, self.patch_size, self.patch_size, 3))
+        rows = tf.split(patch, n, axis=0)
+        rows = [tf.concat(tf.unstack(x), axis=1) for x in rows]
+        reconstructed = tf.concat(rows, axis=0)
+        return reconstructed
+
+# Visualize Patches 
+# Get a batch of images.
+image_batch = next(iter(train_ds))
+
+# Augment the images.
+augmentation_model = get_train_augmentation_model()
+augmented_images = augmentation_model(image_batch)
+
+# Define the patch layer.
+patch_layer = Patches()
+
+# Get the patches from the batched images.
+patches = patch_layer(images=augmented_images)
+
+# Now pass the images and the corresponding patches
+# to the `show_patched_image` method.
+random_index = patch_layer.show_patched_image(images=augmented_images, patches=patches)
+
+# Chose the same chose image and try reconstructing the patches
+# into the original image.
+image = patch_layer.reconstruct_from_patch(patches[random_index])
+plt.imshow(image)
+plt.axis("off")
+plt.show()
+
+# Will create a 2D tensor with shape (batch, sequence_length) (Mask Tensor) and attach it to the tensor output
+# Returned by the Masking and Embedding Layer
+embedding = layers.Embedding(input_dim=5000, output_dim=16, mask_zero=True)
+masked_output = embedding(padded_inputs)
+
+print(masked_output._keras_mask)
+
+masking_layer = layers.Masking()
+# Simulate the embedding lookup by expanding the 2D input to 3D,
+# with embedding dimension of 10.
+unmasked_embedding = tf.cast(
+    tf.tile(tf.expand_dims(padded_inputs, axis=-1), [1, 1, 10]), tf.float32
+)
+
+masked_embedding = masking_layer(unmasked_embedding)
+print(masked_embedding._keras_mask)
